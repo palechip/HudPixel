7e9a31041d5fda662830092660af819aa2cb5698
palechip <palechip@gmail.com>
2014-06-20 20:56:16 +0200

    Add HypixelNetworkDetector and a logger
    - added logging functionality to HudPixelMod
    - listen to the GuiOpenEvent
    - added HypixelNetworkDetector

--- src/main/java/com/palechip/hudpixelmod/HudPixelMod.java
+++ src/main/java/com/palechip/hudpixelmod/HudPixelMod.java
@@ -1,18 +1,74 @@
 package com.palechip.hudpixelmod;
 
-import net.minecraft.init.Blocks;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.palechip.hudpixelmod.detectors.HypixelNetworkDetector;
+
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = HudPixelMod.MODID, version = HudPixelMod.VERSION)
 public class HudPixelMod
 {
     public static final String MODID = "hudpixel";
     public static final String VERSION = "1.0";
+    private static HudPixelMod instance;
+    
+    public static final boolean IS_DEBUGGING = false;
+    public static Logger LOGGER;
+    
+    private HypixelNetworkDetector hypixelDetector;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+    	instance = this;
+    	// initialise the logger
+    	LOGGER = LogManager.getLogger("HudPixel");
+    	
+    	// register this class as a event handler
+    	MinecraftForge.EVENT_BUS.register(this);
+    	
+    	//initialize stuff
+    	this.hypixelDetector = new HypixelNetworkDetector();
+    }
+    
+    @SubscribeEvent
+    public void onGuiShow(GuiOpenEvent event) {
+    	try {
+    		// check if the player is on Hypixel Network
+    		// this can only change when a new gui is opened
+    		this.hypixelDetector.check();
+    	} catch(Exception e) {
+    		this.logWarn("An exception occured in onGuiShow(). Stacktrace below.");
+    		e.printStackTrace();
+    	}
+    }
+    
+    public static HudPixelMod instance() {
+    	return instance;
+    }
+    
+    public void logDebug(String s) {
+    	if(IS_DEBUGGING) {
+    		LOGGER.info("[DEBUG] "  + s);
+    	}
+    }
+    
+    public void logInfo(String s) {
+    	LOGGER.info(s);
+    }
+    
+    public void logWarn(String s) {
+    	LOGGER.warn(s);
+    }
+    
+    public void logError(String s) {
+    	LOGGER.error(s); 	
     }
 }
---
+++ src/main/java/com/palechip/hudpixelmod/detectors/HypixelNetworkDetector.java
@@ -0,0 +1,40 @@
+package com.palechip.hudpixelmod.detectors;
+
+import com.palechip.hudpixelmod.HudPixelMod;
+
+import cpw.mods.fml.client.FMLClientHandler;
+
+public class HypixelNetworkDetector {
+	// saves if the player is online on the Hypixel server
+	public static boolean isHypixelNetwork;
+	
+	private static final String HYPIXEL_DOMAIN = "hypixel.net";
+	
+	/**
+	 * Checks if the Player is on Hypixel Network.
+	 */
+	public void check() {
+		// get the IP of the current server
+		// only if there is one
+		// func_147104_D returns the ServerData of the current server
+		if(FMLClientHandler.instance().getClient().func_147104_D() == null) {
+			// Did the player disconnect?
+			if(isHypixelNetwork) {
+				isHypixelNetwork = false;
+				HudPixelMod.instance().logInfo("Disconnected from Hypixel Network");
+			}
+			return;
+		}
+		String ip = FMLClientHandler.instance().getClient().func_147104_D().serverIP;
+		// if the server ip ends with hypixel.net, it belongs to the Hypixel Network (mc.hypixel.net, test.hypixel.net, maybe mvp.hypixel.net after the new EULA,...)
+		if(!isHypixelNetwork && ip.endsWith(HYPIXEL_DOMAIN)) {
+			isHypixelNetwork = true;
+			HudPixelMod.instance().logInfo("Joined Hypixel Network");
+		}
+		// it can happen that the server data doesn't get null
+		else if(isHypixelNetwork && !ip.endsWith(HYPIXEL_DOMAIN)) {
+			isHypixelNetwork = false;
+			HudPixelMod.instance().logInfo("Disconnected from Hypixel Network");
+		}
+	}
+}
