fec5d61bec49445c5fc7ab7b7c1c663c64de5277
palechip <palechip@gmail.com>
2014-06-24 20:33:28 +0200

    Add timer
    - added TImerComponent
    - implemented it for Quake

---
+++ src/main/java/com/palechip/hudpixelmod/components/TimerComponent.java
@@ -0,0 +1,43 @@
+package com.palechip.hudpixelmod.components;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.palechip.hudpixelmod.HudPixelMod;
+
+import net.minecraft.util.EnumChatFormatting;
+
+public class TimerComponent {
+    public static final String TIME_DISPLAY_MESSAGE = EnumChatFormatting.YELLOW + "Time: ";
+    private long startTime;
+    private String runningTime;
+
+    public String getRenderingString() {
+        return TIME_DISPLAY_MESSAGE + runningTime;
+    }
+    
+    public void setupNewGame() {
+        // reset
+        this.startTime = 0;
+        this.runningTime = "0:00:00";
+    }
+    
+    public void onStartGame() {
+        // save the current time
+        this.startTime = System.currentTimeMillis();
+    }
+    
+    public void onTickUpdate() {
+        // update the time
+        // I tried using Date, but it didn't work out as I wanted so I decided to write this myself
+        long timeDifferenceSeconds, timeDifferenceMinutes, timeDifferenceHours;
+        long timeDifference = System.currentTimeMillis() - this.startTime;
+        // the +500 are for proper rounding
+        timeDifferenceSeconds = (timeDifference + 500) / 1000;
+        timeDifferenceMinutes = timeDifferenceSeconds / 60;
+        timeDifferenceHours = timeDifferenceMinutes / 60;
+        
+        // translate to our format
+        this.runningTime = timeDifferenceHours + ":" + ((timeDifferenceMinutes % 60 < 10) ? "0" : "") + timeDifferenceMinutes % 60 + ":" + ((timeDifferenceSeconds % 60 < 10) ? "0" : "") + timeDifferenceSeconds % 60;
+    }
+}
--- src/main/java/com/palechip/hudpixelmod/games/Quakecraft.java
+++ src/main/java/com/palechip/hudpixelmod/games/Quakecraft.java
@@ -1,28 +1,35 @@
 package com.palechip.hudpixelmod.games;
 
 import com.palechip.hudpixelmod.components.CoinCounterComponent;
+import com.palechip.hudpixelmod.components.TimerComponent;
 
 public class Quakecraft extends Game {
 
-    private static final int COIN_DISPLAY_INDEX = 0;
+    private static final int TIME_DISPLAY_INDEX = 0;
+    private static final int COIN_DISPLAY_INDEX = 1;
 
     CoinCounterComponent coinCounter;
+    TimerComponent timer;
 
     protected Quakecraft() {
         super("Quake", "", "The game is starting! Right-click to shoot with your Railgun to instakill players!", "You earned a total of");
         this.coinCounter = new CoinCounterComponent();
+        this.timer = new TimerComponent();
     }
 
     @Override
     public void setupNewGame() {
         this.coinCounter.setupNewGame();
+        this.timer.setupNewGame();
         
         this.renderStrings.clear();
         this.renderStrings.add(this.coinCounter.getRenderingString());
+        this.renderStrings.add(this.timer.getRenderingString());
     }
 
     @Override
     protected void onGameStart() {
+        this.timer.onStartGame();
     }
 
     @Override
@@ -31,6 +38,8 @@ public class Quakecraft extends Game {
 
     @Override
     public void onTickUpdate() {
+        this.timer.onTickUpdate();
+        this.renderStrings.set(TIME_DISPLAY_INDEX, this.timer.getRenderingString());
     }
 
     @Override
