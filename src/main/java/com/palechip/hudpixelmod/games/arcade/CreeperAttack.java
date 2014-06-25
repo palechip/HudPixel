32b3490ddb44130d6fb556913d51d22be38cd41e
palechip <palechip@gmail.com>
2014-06-25 22:32:39 +0200

    Finalize Creeper Attack

--- src/main/java/com/palechip/hudpixelmod/games/arcade/CreeperAttack.java
+++ src/main/java/com/palechip/hudpixelmod/games/arcade/CreeperAttack.java
@@ -1,42 +1,19 @@
 package com.palechip.hudpixelmod.games.arcade;
 
+import com.palechip.hudpixelmod.HudPixelConfig;
+import com.palechip.hudpixelmod.components.CoinCounterComponent;
+import com.palechip.hudpixelmod.components.TimerComponent;
 import com.palechip.hudpixelmod.games.Game;
 
 public class CreeperAttack extends Game {
 
     public CreeperAttack() {
-        super("", "Creeper Attack", "", "");
-        // TODO Auto-generated constructor stub
+        super("", "Creeper Attack", "The game has started! First wave in 10 seconds", "You earned a total of");
+        if(HudPixelConfig.arcadeTimeDisplay) {
+            this.components.add(new TimerComponent());
+        }
+        if(HudPixelConfig.arcadeCoinDisplay) {
+            this.components.add(new CoinCounterComponent());
+        }
     }
-
-    @Override
-    public void setupNewGame() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    protected void onGameStart() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    protected void onGameEnd() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onTickUpdate() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onChatMessage(String textMessage, String formattedMessage) {
-        // TODO Auto-generated method stub
-
-    }
-
 }
