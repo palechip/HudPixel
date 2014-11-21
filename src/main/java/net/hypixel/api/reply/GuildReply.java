0e01e675875a5b35abee26b3ea67bfc3f424854f
palechip <palechip@gmail.com>
2014-11-21 23:35:56 +0100

    Start API integration

---
+++ src/main/java/net/hypixel/api/reply/GuildReply.java
@@ -0,0 +1,26 @@
+// HypixelAPI (c) 2014
+// Code based on https://github.com/HypixelDev/PublicAPI/commit/0180d6af7c7cb477978c24ba384452e93f30a7b4
+// This is a temporary copyright header which will be replaced when a official header is added.
+
+package net.hypixel.api.reply;
+
+import com.google.gson.JsonObject;
+
+@SuppressWarnings("unused")
+public class GuildReply extends AbstractReply {
+    private JsonObject guild;
+
+    /**
+     * @return The guild object, or null if one wasn't found
+     */
+    public JsonObject getGuild() {
+        return guild;
+    }
+
+    @Override
+    public String toString() {
+        return "GuildReply{" +
+                "guild=" + guild +
+                ",super=" + super.toString() + "}";
+    }
+}
