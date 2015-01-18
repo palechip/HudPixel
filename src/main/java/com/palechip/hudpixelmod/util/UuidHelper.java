bcf3fff656cf141de76adfb8851a7981f91c108f
palechip <palechip@gmail.com>
2015-01-18 15:38:46 +0100

    Enable the mod to get the username from a UUID
    closes #34
    Fixes the booster display sometimes showing "null".

---
+++ src/main/java/com/palechip/hudpixelmod/util/UuidHelper.java
@@ -0,0 +1,37 @@
+package com.palechip.hudpixelmod.util;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.util.UUIDTypeAdapter;
+import com.palechip.hudpixelmod.HudPixelMod;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+/**
+ * A little helper to get the username of a given UUID.
+ * @author palechip
+ */
+public class UuidHelper {
+
+    static int counter;
+    
+    private UuidHelper() {}
+    
+    /**
+     * This should only be called asynchronously since it takes about 150ms to complete on my computer.
+     * @param uuid the UUID to lookup
+     * @return the username or null if it failed
+     */
+    public static String getUsernameFormUUID(String uuid) {
+        if(HudPixelMod.IS_DEBUGGING) {
+            long time = System.currentTimeMillis();
+            // For MC 1.7.10 getSessionService is called func_152347_ac()
+            String name = Minecraft.getMinecraft().getSessionService().fillProfileProperties(new GameProfile(UUIDTypeAdapter.fromString(uuid), (String)null), false).getName();
+            HudPixelMod.instance().logDebug("Took " + (System.currentTimeMillis() - time) + "ms to process UUID. Counter is at " + counter++);
+            return name;
+        } else {
+         // For MC 1.7.10 getSessionService is called func_152347_ac()
+            return Minecraft.getMinecraft().getSessionService().fillProfileProperties(new GameProfile(UUIDTypeAdapter.fromString(uuid), (String)null), false).getName();
+        }
+    }
+}
