c3414ceb0eddde738b0c82cc50fe6f33b7b08be4
palechip <palechip@gmail.com>
2015-02-15 14:58:42 +0100

    Ignore most events on other servers
    - most events now depend on hypixelDetector.isHypixelNetwork
    - also added a few try clauses to newer events
    
    closes #44

--- src/main/java/com/palechip/hudpixelmod/HudPixelMod.java
+++ src/main/java/com/palechip/hudpixelmod/HudPixelMod.java
@@ -117,63 +117,68 @@ public class HudPixelMod
     @SubscribeEvent
     public void onChatMessage(ClientChatReceivedEvent event) {
         try {
-            // this one reads the normal chat messages
-            if(event.type == 0) {
-                // pass the event to the GameDetector
-                this.gameDetector.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
+            //Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // this one reads the normal chat messages
+                if(event.type == 0) {
+                    // pass the event to the GameDetector
+                    this.gameDetector.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
 
-                // pass the chat messages to the current game
-                if(this.gameDetector.getCurrentGame() != null && this.gameDetector.getCurrentGame().hasGameStarted()) {
-                    this.gameDetector.getCurrentGame().onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
-                }
+                    // pass the chat messages to the current game
+                    if(this.gameDetector.getCurrentGame() != null && this.gameDetector.getCurrentGame().hasGameStarted()) {
+                        this.gameDetector.getCurrentGame().onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
+                    }
 
-                // check for start and stop
-                this.gameStartStopDetector.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
+                    // check for start and stop
+                    this.gameStartStopDetector.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
 
-                // pass the message to the api connection
-                this.apiQueue.onChatMessage(event.message.getUnformattedText());
+                    // pass the message to the api connection
+                    this.apiQueue.onChatMessage(event.message.getUnformattedText());
 
-                // and the booster display needs it as well
-                this.renderer.boosterDisplay.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
-            } 
-            // this one are the messages on the status bar
-            else {
-                // not used right now
-            }
-        } catch(Exception e) {
-            this.logWarn("An exception occured in onChatMessage(). Stacktrace below.");
-            e.printStackTrace();
+                    // and the booster display needs it as well
+                    this.renderer.boosterDisplay.onChatMessage(event.message.getUnformattedText(), event.message.getFormattedText());
+                    // this one are the messages on the status bar
+                    else {
+                        // not used right now
+                    }
+                }       
+            } catch(Exception e) {
+                this.logWarn("An exception occured in onChatMessage(). Stacktrace below.");
+                e.printStackTrace();
         }
     }
 
     @SubscribeEvent
     public void onClientTick(ClientTickEvent event) {
         try {
-            // make sure the Scoreboard reader updates when necessary
-            ScoreboardReader.resetCache();
-            
-            // update the resolution and the result display, this renders nothing
-            this.renderer.onClientTick();
-            
-            // pass the event to the GameDetector
-            this.gameDetector.onClientTick();
+            // Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // make sure the Scoreboard reader updates when necessary
+                ScoreboardReader.resetCache();
 
-            if(this.gameDetector.getCurrentGame() != null) {
-                // tick the current game
-                if(this.gameDetector.getCurrentGame().hasGameStarted()) {
-                    this.gameDetector.getCurrentGame().onTickUpdate();
+                // update the resolution and the result display, this renders nothing
+                this.renderer.onClientTick();
+
+                // pass the event to the GameDetector
+                this.gameDetector.onClientTick();
+
+                if(this.gameDetector.getCurrentGame() != null) {
+                    // tick the current game
+                    if(this.gameDetector.getCurrentGame().hasGameStarted()) {
+                        this.gameDetector.getCurrentGame().onTickUpdate();
+                    }
+                    // update render strings
+                    this.gameDetector.getCurrentGame().updateRenderStrings();
                 }
-                // update render strings
-                this.gameDetector.getCurrentGame().updateRenderStrings();
-            }
 
-            this.apiQueue.onClientTick();
-            
-            this.renderer.boosterDisplay.onClientTick();
+                this.apiQueue.onClientTick();
 
-            // check if the update message can be displayed
-            if(this.isUpdateMessageQueued) {
-                this.updateFound();
+                this.renderer.boosterDisplay.onClientTick();
+
+                // check if the update message can be displayed
+                if(this.isUpdateMessageQueued) {
+                    this.updateFound();
+                }
             }
         } catch(Exception e) {
             this.logWarn("An exception occured in onClientTick(). Stacktrace below.");
@@ -184,8 +189,11 @@ public class HudPixelMod
     @SubscribeEvent
     public void onRenderTick(RenderTickEvent event) {
         try {
-            // render the game
-            this.renderer.onRenderTick();
+            // Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // render the game
+                this.renderer.onRenderTick();
+            }
         } catch(Exception e) {
             this.logWarn("An exception occured in onRenderTick(). Stacktrace below.");
             e.printStackTrace();
@@ -194,37 +202,67 @@ public class HudPixelMod
 
     @SubscribeEvent
     public void onKeyInput(KeyInputEvent event) {
-        // check all listened keys
-        if(this.hideHUDKey.isPressed()) {
-            this.renderer.isHUDShown = !this.renderer.isHUDShown;
-        }
-        if(this.openConfigGui.isPressed()) {
-            // open the config screen
-            FMLClientHandler.instance().getClient().displayGuiScreen(new HudPixelConfigGui(null));
+        try {
+            // Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // check all listened keys
+                if(this.hideHUDKey.isPressed()) {
+                    this.renderer.isHUDShown = !this.renderer.isHUDShown;
+                }
+                if(this.openConfigGui.isPressed()) {
+                    // open the config screen
+                    FMLClientHandler.instance().getClient().displayGuiScreen(new HudPixelConfigGui(null));
+                }
+            }
+        } catch(Exception e) {
+            this.logWarn("An exception occured in onClientTick(). Stacktrace below.");
+            e.printStackTrace();
         }
     }
 
     @SubscribeEvent
     public void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent eventArgs) {
-        if(eventArgs.modID.equals(MODID)){
-            this.CONFIG.syncConfig();
-            // reload stuff that uses the config values for immediate effect
-            this.renderer.loadRenderingProperties(updater);
-            Game.loadGames();
+        try {
+            // This event isn't bound to the Hypixel Network
+            if(eventArgs.modID.equals(MODID)){
+                this.CONFIG.syncConfig();
+                // reload stuff that uses the config values for immediate effect
+                this.renderer.loadRenderingProperties(updater);
+                Game.loadGames();
+            }
+        } catch(Exception e) {
+            this.logWarn("An exception occured in onClientTick(). Stacktrace below.");
+            e.printStackTrace();
         }
     }
     
     @SubscribeEvent
     public void onInitGui(InitGuiEvent event) {
-        // pass the event to the booster display
-        // used to inject the tip-all button into GuiChat
-        this.renderer.boosterDisplay.onInitGui(event);
+        try {
+            // Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // pass the event to the booster display
+                // used to inject the tip-all button into GuiChat
+                this.renderer.boosterDisplay.onInitGui(event);
+            }
+        } catch(Exception e) {
+            this.logWarn("An exception occured in onClientTick(). Stacktrace below.");
+            e.printStackTrace();
+        }
     }
     
     @SubscribeEvent
     public void onGuiActionPerformed(ActionPerformedEvent event) {
-        // notify all classes which have registered a button using on init gui
-        this.renderer.boosterDisplay.onGuiActionPerformed(event);
+        try {
+            // Don't do anything unless we are on Hypixel
+            if(this.hypixelDetector.isHypixelNetwork) {
+                // notify all classes which have registered a button using on init gui
+                this.renderer.boosterDisplay.onGuiActionPerformed(event);
+            }
+        } catch(Exception e) {
+            this.logWarn("An exception occured in onClientTick(). Stacktrace below.");
+            e.printStackTrace();
+        }
     }
     
     public void updateFound() {
