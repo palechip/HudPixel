40064248087ea0fb46d9061c0d1d1db229404a56
palechip <palechip@gmail.com>
2015-02-22 15:40:32 +0100

    Correct Warlords GameType
    source:
    https://github.com/Cryptkeeper/hypixel-reference/blob/master/GameType.md

--- src/main/java/com/palechip/hudpixelmod/games/Warlords.java
+++ src/main/java/com/palechip/hudpixelmod/games/Warlords.java
@@ -8,7 +8,7 @@ import net.hypixel.api.util.GameType;
 public class Warlords extends Game {
 
     public Warlords() {
-        super(null, "WARLORDS", START_MESSAGE_DEFAULT, END_MESSAGE_DEFAULT, GameType.WARLORDS , HudPixelConfig.WARLORDS_CATEGORY);
+        super(null, "WARLORDS", START_MESSAGE_DEFAULT, END_MESSAGE_DEFAULT, GameType.BATTLEGROUND , HudPixelConfig.WARLORDS_CATEGORY);
         if(HudPixelConfig.warlordsCoinDisplay) {
             this.components.add(new CoinCounterComponent());
         }
