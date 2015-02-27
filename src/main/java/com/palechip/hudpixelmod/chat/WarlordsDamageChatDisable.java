2baa3f6b972fad71919d76878e7ab585100bf009
MichaelMvv <mikey.tnt.mc@gmail.com>
2015-02-26 19:26:14 -0800

    Fix tabs...

--- src/main/java/com/palechip/hudpixelmod/chat/WarlordsDamageChatDisable.java
+++ src/main/java/com/palechip/hudpixelmod/chat/WarlordsDamageChatDisable.java
@@ -5,24 +5,24 @@ import com.palechip.hudpixelmod.HudPixelConfig;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 
 public class WarlordsDamageChatDisable {
-	public static final String takeDamage = "«";
-	public static final String giveDamage = "»";
+    public static final String takeDamage = "«";
+    public static final String giveDamage = "»";
 
-	public WarlordsDamageChatDisable() {
-	}
+    public WarlordsDamageChatDisable() {
+    }
 
-	public void onChat(ClientChatReceivedEvent e) {
-		if (HudPixelConfig.disableWarlordsDamagePos) {
-			if (e.message.getUnformattedText().startsWith(giveDamage)) {
-				e.setCanceled(true);
-			}
-		}
-		if (HudPixelConfig.disableWarlordsDamageNeg) {
-			if (e.message.getUnformattedText().startsWith(takeDamage)) {
-				e.setCanceled(true);
-			}
-		}
+    public void onChat(ClientChatReceivedEvent e) {
+        if (HudPixelConfig.disableWarlordsDamagePos) {
+            if (e.message.getUnformattedText().startsWith(giveDamage)) {
+                e.setCanceled(true);
+            }
+        }
+        if (HudPixelConfig.disableWarlordsDamageNeg) {
+            if (e.message.getUnformattedText().startsWith(takeDamage)) {
+                e.setCanceled(true);
+            }
+        }
 
-	}
+    }
 
 }
