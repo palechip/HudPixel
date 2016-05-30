b0ba5f8023fed6170930f5205b28f3a4dabbd6e9
unaussprechlich@pc <keck.alexander@web.de>
2016-05-30 06:07:57 +0200

    [update] - updated the project to the newest Forge version [added] - added a ping module to show your current ping while you are ingame

--- src/main/java/com/palechip/hudpixelmod/games/Game.java
+++ src/main/java/com/palechip/hudpixelmod/games/Game.java
@@ -22,11 +22,13 @@
  *******************************************************************************/
 package com.palechip.hudpixelmod.games;
 
-import java.util.ArrayList;
-
 import com.palechip.hudpixelmod.HudPixelMod;
 import com.palechip.hudpixelmod.components.IComponent;
+import com.palechip.hudpixelmod.components.PingComponent;
 import com.palechip.hudpixelmod.util.GameType;
+import de.unaussprechlich.configuration.Config;
+
+import java.util.ArrayList;
 
 public class Game {
     public static final Game NO_GAME = new Game();
@@ -65,17 +67,20 @@ public class Game {
     
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Game) {
+        if (obj instanceof Game) {
             return ((Game) obj).configuration.getModID() == this.configuration.getModID();
-        } else if(obj instanceof GameType) {
-            return ((GameType) obj).getModID() == this.configuration.getModID();
-        } else {
-            return false;
-        }
+        } else
+            return obj instanceof GameType && ((GameType) obj).getModID() == this.configuration.getModID();
     }
 
     public void setupNewGame() {
         this.renderStrings.clear();
+
+        // adds the pingComponent if isPingComponent
+        if(Config.isPingComponent){
+            this.renderStrings.add(PingComponent.getStaticRenderingString());
+        }
+
         for(IComponent component : this.components) {
             component.setupNewGame();
             this.renderStrings.add(component.getRenderingString());
@@ -112,11 +117,11 @@ public class Game {
         if(HudPixelMod.IS_DEBUGGING) {
             renderStrings.add(this.configuration.getShortName() + " " + (this.hasStarted ? "started" : "not started"));
         }
-        for(int i = 0; i < components.size(); i++) {
+        for (IComponent component : components) {
             // only add the string if it actually contains something
             // so if you set the display to bottom, it doesn't float
-            if(!this.components.get(i).getRenderingString().isEmpty()) {
-                this.renderStrings.add(this.components.get(i).getRenderingString());
+            if (!component.getRenderingString().isEmpty()) {
+                this.renderStrings.add(component.getRenderingString());
             }
         }
     }
