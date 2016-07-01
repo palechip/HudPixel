f8133e1cb6bc81060f24d8bde0b29a72dd244851
Elad Kinsbruner <eladkay@gmail.com>
2016-07-01 17:29:03 +0300

    P to play quick change

--- src/main/java/com/palechip/hudpixelmod/modulargui/modules/PlayGameModularGuiProvider.java
+++ src/main/java/com/palechip/hudpixelmod/modulargui/modules/PlayGameModularGuiProvider.java
@@ -45,7 +45,9 @@ public class PlayGameModularGuiProvider extends HudPixelModularGuiProvider {
     @Override
     public String content() {
         if(content.equals("solo_insane") || content.equals("solo_normal") || content.equals("team_insane") || content.equals("team_normal"))
-            return EnumChatFormatting.AQUA + "Press P to play " + EnumChatFormatting.DARK_GREEN + "SkyWars";
+            return EnumChatFormatting.AQUA + "Press P to play " + EnumChatFormatting.BLUE + "SkyWars";
+        if(content.equals("blitz_solo_insane"))
+            return EnumChatFormatting.AQUA + "Press P to play " + EnumChatFormatting.BLUE + "BSG";
         return content;
     }
 
