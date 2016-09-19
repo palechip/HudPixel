aceb98a6ec73c6893d5f3e7bfe5b03f2842992b3
unaussprechlich@pc <keck.alexander@web.de>
2016-09-19 21:47:14 +0200

    some more stuff on the ManagedGuiLib .... the config stuff is almost done ... i still need a way to synchronize local variables with the config! .... BUT FIRST SOME SC TEAMWARS <3

--- src/main/java/de/unaussprechlich/managedgui/lib/elements/ManagedContainer.java
+++ src/main/java/de/unaussprechlich/managedgui/lib/elements/ManagedContainer.java
@@ -1,5 +1,7 @@
 package de.unaussprechlich.managedgui.lib.elements;
 
+import de.unaussprechlich.managedgui.lib.config.ConfigManager;
+
 /******************************************************************************
 
  * Copyright (c) 2016 unaussprechlich
@@ -7,8 +9,37 @@ package de.unaussprechlich.managedgui.lib.elements;
  *******************************************************************************/
 public abstract class ManagedContainer extends Container {
 
+    private final String ID;
+
+    public enum ManagedContainerDefaultProperties{
+
+        X_OFFSET("xOffset"),
+        Y_OFFSET("yOffset"),
+        WIDTH("width"),
+        HEIGHT("height"),
+        COLOR_R("colorRed"),
+        COLOR_B("colorBlue"),
+        COLOR_G("colorGreen"),
+        COLOR_A("colorAlpha"),
+        FLOAT_X("floatX"),
+        FLOAT_Y("floatY");
+
+        final String name;
+        ManagedContainerDefaultProperties(String name){
+            this.name = name;
+        }
+    }
 
     public ManagedContainer(String name) {
-        super(name);
+        this.ID = name;
+        ConfigManager.registerCategory(name);
+    }
+
+    public <T> void registerProperty(String name, T defaultValue){
+        ConfigManager.getCategoryByName(ID).registerProperty(name, defaultValue);
+    }
+
+    public <T> T getProperty(String name){
+        return ConfigManager.getCategoryByName(ID).getPopertyValueByName(name);
     }
 }
